55d82c2ef351e58ad027f9bd60f8b76a81d70249
MtLuth <111564276+MtLuth@users.noreply.github.com>
2023-12-20 12:17:29 +0700

    done edit

--- src/main/java/alotra/milktea/controller/HomeController.java
+++ src/main/java/alotra/milktea/controller/HomeController.java
@@ -9,7 +9,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.Banner;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
